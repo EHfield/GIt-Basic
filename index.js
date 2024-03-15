b6d2fc0413dead55ac751e85d5f0717fd8b4518f
EHfield <mailmeyourletter@gmail.com>
2024-03-15 15:47:07 +0900

    login user name arg add

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
     index.html 에서 참조하는 스크립트이다
 */
 
-function login() {
-    console.log("로그인")
+function login(username) {
+    if(username){
+        console.log("로그인")
+    }else {
+        console.log('username을 입력하세요!!')
+    }
 }
 function logout() {
     console.log("로그아웃")
